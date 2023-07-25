2962de9f4538b8ad90b70e0f2d49ee3519866410
GArdennes <thekevin.afachao@gmail.com>
2023-07-25 20:45:13 +0200

    Updates to commit

--- New_folder4/info2.c
+++ New_folder4/info2.c
@@ -23,3 +23,22 @@ void free_list(list_t **head_ptr)
 	}
 	*head_ptr = NULL;
 }
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: the destination buffer
+ * @src: the source buffer
+ *
+ * Return: pointer to destination buffer
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ret = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret);
+}
--- New_folder4/input2.c
+++ New_folder4/input2.c
@@ -106,3 +106,18 @@ char *find_path(info_t *info, char *pathstr, char *cmd)
 	}
 	return (NULL);
 }
+
+/**
+ * starts_with - checks if needle starts with haystack
+ * @haystack: string to search
+ * @needle: the substring to find
+ *
+ * Return: address of next char of haystack or NULL
+ */
+char *starts_with(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
